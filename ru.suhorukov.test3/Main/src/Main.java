d8c71f36dad7e719cc9e3e4a2d449135e0ac4405
plasmorf <plasmorf@gmail.com>
2015-09-25 12:37:53 +0700

    3

--- ru.suhorukov.test3/Main/src/Main.java
+++ ru.suhorukov.test3/Main/src/Main.java
@@ -1,14 +1,20 @@
 import java.io.*;
-import java.util.HashMap;
 
 /**
  * Created by Администратор on 24.09.2015.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        Reader r = new InputStreamReader(new BufferedInputStream(new FileInputStream(String.join(" ", args))));
-        MyList l = new MyList();
-        l.scan(r);
+        try(Reader r = new InputStreamReader(new BufferedInputStream(new FileInputStream(args[0])))) { //String.join(" ", args)
+            MyList l = new MyList();
+            l.setList(InOut.scan(r));
+            l.sort();
 
+            if (args[1] != ""){
+                Writer w = new OutputStreamWriter(new FileOutputStream(args[1]));
+
+            }
+            l.getMyList();
+        }
     }
 }
